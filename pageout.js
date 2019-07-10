8f1a271f610a4c59c2e9e1ad791684202532288d
Lisa Yang <30415240+lisa-yang12@users.noreply.github.com>
2019-07-10 10:40:24 -0400

    Update pageout.js

--- pageout.js
+++ pageout.js
@@ -215,8 +215,6 @@ function resolveIncident() {
 function main() {
     $('#login').click(function(e) {
         requestOAuthToken();
-         console.log("Refreshing Page");
-         location.reload(true);
     });
     $('#logout').click(function(e) {
         removeOAuthToken();
@@ -237,7 +235,8 @@ function main() {
         } else {
             // page load when being redirected from PagerDuty OAuth service
             receiveOAuthToken(oauthResponseParams);
-           
+            console.log("Refreshing Page");
+            location.reload(true);
             $('#content').show();
             $('#logout').show();
             $('#login').hide();
@@ -245,7 +244,7 @@ function main() {
     }
 
     $('#login').hide();
-
+    
     //$('#incident-text').attr("placeholder", default_incident_summary);
     //populateUserSelect();
     //populateServiceSelect();
